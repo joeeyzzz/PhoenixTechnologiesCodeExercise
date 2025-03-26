b30f3a891addbcd0ac3c750bed1b451ef0c6b6f8
José Luis Gutiérrez Álvarez <73435992+joeeyzzz@users.noreply.github.com>
2025-03-26 18:23:12 +0100

    Main and Spring AppConfig

---
+++ src/main/java/com/joseluis/PhoenixTechnologiesCodeExercise/PhoenixTechnologiesCodeExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.joseluis.PhoenixTechnologiesCodeExercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoenixTechnologiesCodeExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoenixTechnologiesCodeExerciseApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/joseluis/PhoenixTechnologiesCodeExercise/config/AppConfig.java
@@ -0,0 +1,50 @@
+package com.joseluis.PhoenixTechnologiesCodeExercise.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class AppConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/**").permitAll()
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .requestMatchers(
+                                "/swagger-ui.html",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/swagger-resources/**",
+                                "/webjars/**",
+                                "/api-docs/swagger-config",
+                                "/api-docs"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .headers(headers -> headers
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
+                );
+        return http.build();
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+
+        modelMapper.getConfiguration()
+                .setFieldMatchingEnabled(true)
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE);
+
+        return modelMapper;
+    }
+}
